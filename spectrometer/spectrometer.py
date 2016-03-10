36a5dddad4f0aa0d8726554862dd66f1fc1f6bdb
Catherine Holloway <milankie@gmail.com>
2016-03-10 14:29:01 -0800

    removed pipes from init_jvm

--- spectrometer/spectrometer.py
+++ spectrometer/spectrometer.py
@@ -10,57 +10,47 @@ class SpectrometerException(Exception):
     pass
 
 
-def init_jvm(pipe_to_parent):
+def init_jvm(pipe_to_parent=None):
     """
     init_jvm starts the gateway java instance
     :return:
     """
-    try:
-        # first, setup and test the file locations - change these to your system
+    # first, setup and test the file locations - change these to your system
+    od_jar_locations = \
+        "C:\\Program Files\\Ocean Optics\\OmniDriver\\OOI_HOME"
+    java_location = "C:\\Program Files\\Java\\jre1.8.0_65\\bin\\java.exe"
+    py4j_location = "c:\\Python27\\share\\py4j\\py4j0.9.jar"
 
-        od_jar_locations = \
-            "C:\\Program Files\\Ocean Optics\\OmniDriver\\OOI_HOME"
-        java_location = "C:\\Program Files\\Java\\jre1.8.0_65\\bin\\java.exe"
-        py4j_location = "c:\\Python27\\share\\py4j\\py4j0.9.jar"
+    if not os.path.isdir(od_jar_locations):
+        raise ValueError("OmniDriver directory: "+od_jar_locations +
+                         " does not exist")
 
-        if not os.path.isdir(od_jar_locations):
-            raise ValueError("OmniDriver directory: "+od_jar_locations +
-                             " does not exist")
+    if not os.path.exists(java_location):
+        raise ValueError("Java not found at: "+java_location)
+    if not os.path.exists(py4j_location):
+        raise ValueError("Py4J not found at: "+py4j_location)
 
-        if not os.path.exists(java_location):
-            raise ValueError("Java not found at: "+java_location)
-        if not os.path.exists(py4j_location):
-            raise ValueError("Py4J not found at: "+py4j_location)
+    jars = [os.path.join(od_jar_locations, _file)
+            for _file in os.listdir(od_jar_locations)
+            if _file.endswith(".jar")]
 
-        jars = [os.path.join(od_jar_locations, _file)
-                for _file in os.listdir(od_jar_locations)
-                if _file.endswith(".jar")]
-
-        # this assumes that your .class file has been compiled to the same
-        # folder as this script
-        file_encoding = "-Dfile.encoding=Cp1252"
-        jars.append(py4j_location)
-        classpath = os.path.dirname(__file__)+";"+";".join(jars)
-        parts = [java_location, file_encoding, "-classpath", classpath,
-                 "SpectrometerServer"]
-        call(parts)
-    except Exception as e:
-        pipe_to_parent.send((type(e), e))
+    # this assumes that your .class file has been compiled to the same
+    # folder as this script
+    file_encoding = "-Dfile.encoding=Cp1252"
+    jars.append(py4j_location)
+    classpath = os.path.dirname(__file__)+";"+";".join(jars)
+    parts = [java_location, file_encoding, "-classpath", classpath,
+             "SpectrometerServer"]
+    call(parts)
 
 
 class Spectrometer(JavaGateway):
     def __init__(self):
         super(self.__class__, self).__init__()
-        to_child, to_self = Pipe()
-        p = Process(target=init_jvm, args=(to_self,))
+        #to_child, to_self = Pipe()
+        p = Process(target=init_jvm)
         p.start()
-        # check on how the java connection went
-        exc_info = to_child.recv()
-        # if the startup did not go well, exit
-        if exc_info[0] == ValueError:
-            raise SpectrometerException("pyOmniDriver failed: "+str(exc_info[
-                                                                        1]))
-            return
+
         # wait for the process to start
         connection = False
         out_str = ""
